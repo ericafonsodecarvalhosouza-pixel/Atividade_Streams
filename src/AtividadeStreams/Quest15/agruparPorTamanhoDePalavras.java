9d603bf6d6095972b81389c8bd12f5a84ee9db35
ericafonsodecarvalhosouza-pixel <ericafonsodecarvalhosouza@gmail.com>
2026-05-02 16:57:22 -0300

    Atividade Streams

--- src/AtividadeStreams/Quest15/agruparPorTamanhoDePalavras.java
+++ src/AtividadeStreams/Quest15/agruparPorTamanhoDePalavras.java
@@ -1,6 +1,24 @@
 package AtividadeStreams.Quest15;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class agruparPorTamanhoDePalavras {
 
+    private static List<String> palavras = new ArrayList<>(Arrays
+            .asList("sol", "casa", "computador", "ar", "desenvolvimento",
+                    "lua", "java", "programação", "céu", "tecnologia",
+                    "ia", "algoritmo", "web", "estrutura", "dados"
+            ));
+
+    static void main(String[] args) {
+
+        var listaPorTamanhoDaPalavra = palavras.stream().collect(Collectors.groupingBy(p -> p.length()));
+
+        System.out.println(listaPorTamanhoDaPalavra);
+
+    }
 
 }
